c49e4a8cbb7e2e0f8ef002ca3498d4bc1fb6e582
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2018-06-19 23:49:47 -0400

    Added NotifyModel.

---
+++ SpeckyStandard/Models/NotifyBase.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SpeckyStandard.Models
+{
+    public class NotifyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void Notify([CallerMemberName] string callerName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(callerName));
+    }
+}
