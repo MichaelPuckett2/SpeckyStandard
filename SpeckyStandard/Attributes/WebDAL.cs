35ab58fc4c06fd674ebfa18b5e00fe424f129555
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2018-06-21 16:24:27 -0400

    First commit... on hold.

---
+++ SpeckyStandard/Attributes/WebDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SpeckyStandard.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+    public sealed class WebDAL : Attribute
+    {
+        public string Url { get; }
+        public int Interval { get; }
+
+        public WebDAL(string url) : this(url, 0) { }
+
+        public WebDAL(string url, int interval) 
+        {
+            Url = url;
+            Interval = interval;
+        }
+    }
+}
