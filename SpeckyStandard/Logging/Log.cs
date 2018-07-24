2973e68239eb17a60796bdf731fd619dbdb723d5
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2018-07-24 02:43:54 -0400

    Changed namespace name from Debug to Logging so that it's not confused with other commonly used Debug folder names and namespaces.

---
+++ SpeckyStandard/Logging/Log.cs
@@ -0,0 +1,29 @@
+﻿using SpeckyStandard.Enums;
+using System;
+
+namespace SpeckyStandard.Logging
+{
+    internal static class Log
+    {
+        public static void Print(string message, PrintType printType, Exception innerException = null)
+        {
+            if (printType == PrintType.DebugWindow)
+            {
+                System.Diagnostics.Debug.Print(message);
+            }
+
+            if (printType == PrintType.LogFile)
+            {
+                throw new NotImplementedException($"{nameof(Log)}.{nameof(Print)} does not yet support {nameof(PrintType.LogFile)}");
+            }
+
+            if (printType == PrintType.ThrowException)
+            {
+                if (innerException != null)
+                    throw innerException == null 
+                        ? throw new Exception(message) 
+                        : new Exception(message, innerException);
+            }
+        }
+    }
+}
