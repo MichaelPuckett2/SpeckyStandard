2e38fae4e900f4383add39d94dcaf2d2d8514061
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2018-07-24 12:37:09 -0400

    Removed unneeded disposable.

--- SpeckyStandard/Commands/CommandBindings.cs
+++ SpeckyStandard/Commands/CommandBindings.cs
@@ -4,22 +4,11 @@ using System.ComponentModel;
 
 namespace SpeckyStandard.Commands
 {
-    public sealed class CommandBindings : IDisposable
+    public sealed class CommandBindings
     {
         public Action<object> ExecuteAction { get; set; }
         public List<Predicate<object>> CanExecutePredicates { get; set; }
         public INotifyPropertyChanged BindingModel { get; set; }
         public List<string> BindingPropertNames { get; set; }
-
-        public void Dispose()
-        {
-            ExecuteAction = null;
-            CanExecutePredicates = null;
-            BindingModel = null;
-            BindingPropertNames = null;
-            GC.SuppressFinalize(this);
-        }
-
-        ~CommandBindings() => Dispose();
     }
 }
