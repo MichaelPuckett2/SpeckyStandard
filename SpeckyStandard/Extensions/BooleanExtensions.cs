388fc0b4eaa859e3da7df0ea7bf8c029922f979d
Michael Puckett <michael.puckett.ii@outlook.com>
2018-12-10 12:14:34 -0500

    Updated BooleanExtensions and Pulse methods.

--- SpeckyStandard/Extensions/BooleanExtensions.cs
+++ SpeckyStandard/Extensions/BooleanExtensions.cs
@@ -12,44 +12,34 @@ namespace SpeckyStandard.Extensions
         /// <param name="False">Action to invoke when boolean is false.</param>
         public static bool Pulse(this bool boolean, Action True = null, Action False = null)
         {
-            if (boolean) True?.Invoke();
-            else False?.Invoke();
+            if (boolean)
+            {
+                True?.Invoke();
+            }
+            else
+            {
+                False?.Invoke();
+            }
             return boolean;
-        }
-
-        /// <summary>
-        /// Invokes a predicate when boolean is true.
-        /// </summary>
-        /// <param name="boolean"></param>
-        /// <param name="predicate">Predicate to invoke when boolean is true.</param>
-        /// <returns>Returns the result of the invoked predicate.</returns>
-        public static bool PulseOnTrue(this bool boolean, Predicate<bool> predicate)
-        {
-            if (boolean) return predicate.Invoke(boolean); else return boolean;
-        }
-
-        /// <summary>
-        /// Invokes a predicate when boolean is false.
-        /// </summary>
-        /// <param name="boolean"></param>
-        /// <param name="predicate">Predicate to invoke when boolean is false.</param>
-        /// <returns>Returns the result of the invoked predicate.</returns>
-        public static bool PulseOnFalse(this bool boolean, Predicate<bool> predicate)
-        {
-            if (!boolean) return predicate.Invoke(boolean); else return boolean;
         }
 
         /// <summary>
         /// Invokes seperate predicates when boolean is true or false.
         /// </summary>
         /// <param name="boolean"></param>
-        /// <param name="truePredicate">Predicate to invoke when boolean is true.</param>
-        /// <param name="falsePredicate">Predicate to invoke when boolean is false.</param>
-        /// <returns>Returns the result of the invoked predicate.</returns>
-        public static bool Pulse(this bool boolean, Predicate<bool> truePredicate = null, Predicate<bool> falsePredicate = null)
+        /// <param name="True">Predicate to invoke when boolean is true.  The result of the predicate will be the return value of the Pulse. If no predicate is supplied it will return the original boolean value.</param>
+        /// <param name="False">Predicate to invoke when boolean is false. The result of the predicate will be the return value of the Pulse. If no predicate is supplied it will return the original boolean value.</param>
+        /// <returns>Returns the result of the invoked predicate assocaited with if the boolean is true or false, if a predicate is supplied, otherwise returns the original boolean value.</returns>
+        public static bool Pulse(this bool boolean, Predicate<bool> True = null, Predicate<bool> False = null)
         {
-            if (boolean) return boolean.PulseOnTrue(truePredicate);
-            else return boolean.PulseOnFalse(falsePredicate);
+            if (boolean)
+            {
+                return True?.Invoke(boolean) ?? boolean;
+            }
+            else
+            {
+                return False?.Invoke(boolean) ?? boolean;
+            }
         }
     }
 }
